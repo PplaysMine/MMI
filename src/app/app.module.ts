52c429664c7cf1a45ef7f957ae3991bfc422b778
paulwiegand <pwiegand2001@gmail.com>
2022-10-27 09:19:20 +0200

    Add main view & menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MainViewComponent } from './views/main-view/main-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule,
